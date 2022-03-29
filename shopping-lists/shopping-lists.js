d11a3cefb1cd7d92ee184ec080916965b72ef678
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-29 11:41:51 -0700

    added correct element being appended on line 55

--- shopping-lists/shopping-lists.js
+++ shopping-lists/shopping-lists.js
@@ -52,7 +52,7 @@ async function fetchAndDisplayList() {
             fetchAndDisplayList();
         });
 
-        shoppingListEl.append(shoppingListEl);
+        shoppingListsEl.append(shoppingListEl);
     }
 }
 
