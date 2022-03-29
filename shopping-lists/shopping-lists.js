8706ddc7b4ab29c5bbf056562395e2650e7a2005
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-29 10:59:07 -0700

    hardcoded html in shopping-lists/index.html.

--- shopping-lists/shopping-lists.js
+++ shopping-lists/shopping-lists.js
@@ -1,3 +1,4 @@
+//import necessary files from respective files
 import {
     checkAuth,
     createItem,
@@ -9,6 +10,8 @@ import {
 import { renderItem } from '../render-utils.js';
 
 checkAuth();
+
+const shoppingListsEl = document.querySelector('')
 
 const logoutButton = document.getElementById('logout');
 
