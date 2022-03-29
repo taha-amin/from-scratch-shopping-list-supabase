3a423939c13ac89fcbbeea7d48e0d30d104ee77e
Mohamed Amin <mohamedamin2009ster@gmail.com>
2022-03-29 10:34:33 -0700

    created func renderItem(item)

---
+++ render-utils.js
@@ -0,0 +1,20 @@
+export function renderItem(item) {
+    //create div and p tag
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+
+    //depending on whether the shopping list is complete, give the div and the appropriate css class 'complete' or 'incomplete'
+    div.classList.add(item.complete ? 'complete' : 'incomplete');
+
+    //add the 'item' css class no matter what
+    div.classList.add('item');
+
+    //put the shopping lists text into the p tag
+    p.textContent = item.item;
+
+    //append stuff
+    div.append(p);
+
+    //return the div
+    return div;
+}
